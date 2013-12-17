f005cd623a5a50d747a6faf7d837a34f64d0bcfc
Tobias Becker <rh-clash@web.de>
2013-12-17 14:26:30 +0100

    Moved bubble sort to the library.

---
+++ SortLib/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using SortLib;
+using System.ComponentModel.Composition;
+
+namespace SortVis
+{
+    /// <summary>
+    /// Implementation of bubble sort.
+    /// </summary>
+    /// <remarks>
+    /// Implemented in executing assembly so that at least one sorter is always found.
+    /// </remarks>
+    [Export(typeof(ISorter))]
+    [ExportMetadata("Name", "Bubblesort")]
+    public class BubbleSort : SorterBase
+    {
+        /// <summary>
+        /// Do the actual sorting work.
+        /// </summary>
+        protected override void SortIt()
+        {
+            int n = Numbers.Length;
+            do
+            {
+                SortedFrom = n;
+                int newN = 1;
+                for (int i = 0; i < n - 1; ++i)
+                {
+                    Abort.ThrowIfCancellationRequested();
+                    if (CompareInArray(i, i + 1) > 0)
+                    {
+                        Swap(i, i + 1);
+                        newN = i + 1;
+                    }
+                }
+                n = newN;
+            } while (n > 1);
+        }
+    }
+}
