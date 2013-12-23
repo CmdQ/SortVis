3b4ccac0b3592e5afc9fe7845d894d7369a0a93d
Tobias Becker <rh-clash@web.de>
2013-12-23 13:12:33 +0100

    Added a C++ project to play with radix sort.

---
+++ RadixSort/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "radix.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Radix sort test.\n";
+    return 0;
+}
---
+++ RadixSort/radix.h
@@ -0,0 +1,6 @@
+#ifndef _RADIX_H_
+
+#include <limits>
+
+#define _RADIX_H_
+#endif//_RADIX_H_
