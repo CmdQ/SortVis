fbdbe1d076469f4d9014bcb81c19e01e5f33df1f
Tobias Becker <rh-clash@web.de>
2014-01-10 13:47:34 +0100

    Added scaffold for a red-black tree.

---
+++ SortLib/RedBlackTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLib
+{
+    /// <summary>
+    /// A left-leaning variant of a red-black tree. Especially wrt. deletion it's
+    /// supposed to be easier to implement.
+    /// </summary>
+    public class RedBlackTree
+    {
+    }
+}
